7b27c9c5e75023152f1e6fc35ef65beb161eb729
Vanessa Batista <vanessaboliver02@gmail.com>
2026-02-23 19:30:09 -0300

    teste: change message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('Hello DevOps 🚀');
+  res.end('Hello batistão 🚀');
 });
 
 server.listen(port, '0.0.0.0', () => {
