7a4d3930bc3977433f51abddf5faea7c3dedcc4b
Vanessa Batista <vanessaboliver02@gmail.com>
2026-02-23 17:50:54 -0300

    initial devops lab app

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Hello DevOps 🚀');
+});
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`App running on port ${port}`);
+});
