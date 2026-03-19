2c119524d289983adcbbe52e32f8f5231b588c1d
Vanessa Batista <vanessaboliver02@gmail.com>
2026-03-19 10:43:44 -0300

    alteracao nome app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('Hello batistão 🚀');
+  res.end('Hello Sosinho 🚀');
 });
 
 server.listen(port, '0.0.0.0', () => {
